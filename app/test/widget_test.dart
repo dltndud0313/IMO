58cc030b43fd3214801eb3d8b9b91a4fdd653d1f
이진원 <ljin2091@naver.com>
2026-04-20 11:53:07 +0900

    chore: rename package to inside_muscle_out

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_1/main.dart';
+import 'package:inside_muscle_out/main.dart';
 
 void main() {
   testWidgets('App boots and shows home screen', (WidgetTester tester) async {
